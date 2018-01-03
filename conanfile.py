66842df9f6ed733a9b1f3268793137c3a9d7ad97
Alexander Zaitsev <zamazan4ik@tut.by>
2018-01-03 16:42:16 +0300

    Add initial version

---
+++ conanfile.py
@@ -0,0 +1,25 @@
+from conans import ConanFile, CMake, tools
+import os
+
+
+class EasyLoggingConan(ConanFile):
+    name = "easyloggingpp"
+    version = "9.95.3"
+    license = "MIT"
+    url = "https://github.com/bincrafters/conan-easyloggingpp"
+    description = """Single header C++ logging library. It is extremely powerful, extendable, 
+light-weight, fast performing, thread and type safe and consists of many built-in features. 
+It provides ability to write logs in your own customized format.
+It also provide support for logging your classes, third-party libraries, STL and third-party containers etc."""
+
+    def source(self):
+        tools.download("https://github.com/muflihun/easyloggingpp/releases/download/v%s/easyloggingpp_v%s.zip" % (self.version, self.version),
+                       "easyloggingpp.zip")
+        tools.unzip("easyloggingpp.zip" )
+
+    def package(self):
+        self.copy("*.h", "include")
+        self.copy("*.cc", "include")
+
+
+
